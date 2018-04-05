1cdc95b454407f192a1846e964689a3e7933fa2c
alumne <alumne@10.0.2.5>
2018-04-05 20:56:52 +0200

    Development

--- src/Exercici/Vehicle.java
+++ src/Exercici/Vehicle.java
@@ -2,8 +2,23 @@ package Exercici;
 
 
 public class Vehicle{
-	private int cilindrada;
-	private int portes;
-	private int velocitat_actual;
+	public int cilindrada;
+	public int portes;
+	public static int velocitat_actual;
 	public static final int velocitat_maxima = 120;
+	public static void accelerar() {
+		if (velocitat_actual < velocitat_maxima) {
+			velocitat_actual += 10;
+		}else {
+			System.out.println("Ya se ha alcanzado la velocidad maxima");
+		}
+	}
+	
+	public static void desaccelerar() {
+		if(velocitat_actual>0) {
+			velocitat_actual-=10;
+		}else {
+			System.out.println("El vehiculo ya esta parado.");
+		}
+	}
 }
