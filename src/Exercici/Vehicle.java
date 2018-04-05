d8ef45db73877067c32084ba61f9a08bc00fe0ea
alumne <alumne@10.0.2.5>
2018-04-05 20:59:03 +0200

    Segunda modificacion rama master

--- src/Exercici/Vehicle.java
+++ src/Exercici/Vehicle.java
@@ -6,4 +6,10 @@ public class Vehicle{
 	private int portes;
 	private int velocitat_actual;
 	public static final int velocitat_maxima = 120;
+	public Vehicle(int cilindrada, int portes) {
+		super();
+		this.cilindrada = cilindrada;
+		this.portes = portes;
+		this.velocitat_actual = 0;
+	}
 }
