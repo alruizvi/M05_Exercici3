02b2c4280a32d5d1c349c822d56309ce7c598713
alumne <alumne@10.0.2.5>
2018-04-09 16:09:23 +0200

    Marxes afegides

--- src/Exercici/Vehicle.java
+++ src/Exercici/Vehicle.java
@@ -5,7 +5,9 @@ public class Vehicle{
 	public int cilindrada;
 	public int portes;
 	public static int velocitat_actual;
+	public static int marxaActual = 0;
 	public static final int velocitat_maxima = 120;
+	public static final int marxes = 5;
 	
 	public Vehicle(int cilindrada, int portes) {
 		super();
@@ -29,4 +31,23 @@ public class Vehicle{
 			System.out.println("El vehiculo ya esta parado.");
 		}
 	}
+	
+	public static void pujaMarxa() {
+		if (marxaActual >= marxes) {
+			marxaActual = marxes;
+			System.out.println("No es pot pujar la marxa");
+		}else {
+			marxaActual += 1;
+		}
+	}
+	
+	public static void baixaMarxa() {
+		if(marxaActual <= 0) {
+			marxaActual = 0;
+			System.out.println("No es pot baixar la marxa");
+		}else {
+			marxaActual -= 1;
+		}
+	}
+	
 }
